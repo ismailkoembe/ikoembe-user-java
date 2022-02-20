d2bd0ed341bada51b6c04f618dbe8fba477f72c2
ismailkombe <Fimado839092>
2022-02-20 23:09:17 +0100

    As of now, students can not have other roles Signup endpoint is deprecated User createdDate is handled by code itself

--- src/main/java/com/ikoembe/study/user/models/ERole.java
+++ src/main/java/com/ikoembe/study/user/models/ERole.java
@@ -1,8 +1,15 @@
 package com.ikoembe.study.user.models;
 
+import java.security.PrivateKey;
+
 public enum ERole {
   ROLE_ADMIN,
   ROLE_STUDENT,
   ROLE_GUARDIAN,
-  ROLE_TEACHER
+  ROLE_TEACHER;
+  private String name;
+
+  public String getName() {
+    return name;
+  }
 }
