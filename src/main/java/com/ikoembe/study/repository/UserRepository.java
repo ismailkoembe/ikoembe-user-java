e0bbc40c491383d3d006a28a33de3dc30faea4f2
ismailkombe <Fimado839092>
2022-02-24 23:59:13 +0100

    when user logs in, we record lastSignIn time in DB

--- src/main/java/com/ikoembe/study/repository/UserRepository.java
+++ src/main/java/com/ikoembe/study/repository/UserRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-  Optional<User> findByUsername(String username);
+  User findByUsername(String username);
 
   Boolean existsByUsername(String username);
 
