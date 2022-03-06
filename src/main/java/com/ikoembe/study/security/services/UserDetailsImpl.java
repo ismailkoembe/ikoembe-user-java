13857d716b1f63ae6cfa97ea0768c483ad04bceb
ismailkombe <Fimado839092>
2022-03-06 17:42:30 +0100

    when user logs in, we record lastSignIn time in DB

--- src/main/java/com/ikoembe/study/security/services/UserDetailsImpl.java
+++ src/main/java/com/ikoembe/study/security/services/UserDetailsImpl.java
@@ -40,7 +40,7 @@ public class UserDetailsImpl implements UserDetails {
 				.collect(Collectors.toList());
 
 		return new UserDetailsImpl(
-				user.getId(), 
+				user.getAccountId(),
 				user.getUsername(), 
 				user.getEmail(),
 				user.getPassword(), 
