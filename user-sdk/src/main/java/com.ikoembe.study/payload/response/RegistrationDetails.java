cec150b74506caa42e1577a83bdafd752c3f4a9e
ismailkombe <Fimado839092>
2022-09-17 01:04:27 +0200

    make the api return Thomas style Add userms sdk

---
+++ user-sdk/src/main/java/com.ikoembe.study/payload/response/RegistrationDetails.java
@@ -0,0 +1,18 @@
+package com.ikoembe.study.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter @Setter @AllArgsConstructor
+public class RegistrationDetails {
+    private String firstname;
+    private String lastname;
+    private String middlename;
+    private String username;
+    private String temporarilyPass;
+}
