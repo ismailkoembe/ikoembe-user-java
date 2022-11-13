9daeadfe4f7443b1019aa1ec7a43ac4fba6bc467
ismailkombe <Fimado839092>
2022-11-13 23:02:13 +0100

    user-sdk

--- user-sdk/src/main/java/model/User.java
+++ user-sdk/src/main/java/model/User.java
@@ -1,10 +1,20 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+
+@Slf4j @Getter @Setter @AllArgsConstructor
 public class User {
+
     private String accountId;
 
     private String username;
@@ -15,10 +25,9 @@ public class User {
 
     private String lastname;
 
-    private String email;
-
     private Set<Roles> roles = new HashSet<>();
 
     private LocalDate birthdate;
+
 
 }
